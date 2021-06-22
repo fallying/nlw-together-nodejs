7e9cb9a708cdf6f228c055f653d8b35faa3eb91b
fallying <jonasexplore@gmail.com>
2021-06-22 20:23:51 -0300

    fix(users): move to new path

---
+++ src/modules/users/useCases/CreateUser/CreateUserController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { IUserRequest } from "../../dtos";
+import { CreateUser } from "./CreateUser";
+
+class CreateUserController {
+  async handle(request: Request, response: Response) {
+    const { email, name, admin }: IUserRequest = request.body;
+
+    const createUser = new CreateUser();
+
+    const user = await createUser.execute({name, email, admin});
+
+    return response.json(user);
+
+  }
+}
+
+export {CreateUserController}
---
+++ src/modules/users/useCases/CreateUser/index.ts
@@ -0,0 +1,2 @@
+export {CreateUser} from './CreateUser'
+export {CreateUserController} from './CreateUserController'
