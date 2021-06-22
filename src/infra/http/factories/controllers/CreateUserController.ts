d4147f4daa5a9beb5ef5ab36054e2cfa499c878a
fallying <jonasexplore@gmail.com>
2021-06-21 21:31:21 -0300

    feat(infra): created routes and controllers

---
+++ src/infra/http/factories/controllers/CreateUserController.ts
@@ -0,0 +1,5 @@
+import { CreateUserController } from "../../../../modules/users/useCases/CreateUserController";
+
+const createUserController = new CreateUserController();
+
+export {createUserController}
