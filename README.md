87ec7cfeac5eaabd327daa4469e2356a91354d86
fallying <jonasexplore@gmail.com>
2021-06-22 21:18:16 -0300

    fix(*): fixed format indentation

--- README.md
+++ README.md
@@ -3,13 +3,15 @@
 ## Rules
 
 - Users Registration
+
   - [x] Do not allowed duplicate email registration.
   - [x] Do not allowed user registration with null email.
 
 - Tag Registration
-  - [ ] Do not allowed duplicate tag registration.
-  - [ ] Do not allowed to register an unnamed tag.
-  - [ ] Do not allowed to register tag a tag by non-admin users.
+
+  - [x] Do not allowed duplicate tag registration.
+  - [x] Do not allowed to register an unnamed tag.
+  - [x] Do not allowed to register tag a tag by non-admin users.
 
 - Compliments Registration
   - [ ] Do not allowed to register a compliment for themselves.
--- src/modules/users/useCases/CreateUser/CreateUser.ts
+++ src/modules/users/useCases/CreateUser/CreateUser.ts
@@ -1,33 +1,34 @@
 import { getCustomRepository } from "typeorm";
+import { User } from "../../domain";
 import { IUserRequest } from "../../dtos";
 import { UsersRepositories } from "../../repositories";
 
 class CreateUser {
- async execute({email, name, admin}: IUserRequest) {
-  const usersRepository = getCustomRepository(UsersRepositories);
+  async execute({ email, name, admin }: IUserRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepositories);
 
-  if (!email) {
-    throw new Error("Invalid email");
+    if (!email) {
+      throw new Error("Invalid email");
+    }
+
+    const userAlreadyExists = await usersRepository.findOne({
+      email,
+    });
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+
+    const user = usersRepository.create({
+      name,
+      email,
+      admin,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
   }
-
-  const userAlreadyExists = await usersRepository.findOne({
-    email
-  })
-
-  if (userAlreadyExists) {
-    throw new Error("User already exists");
-  }
-
-  const user = usersRepository.create({
-    name, 
-    email, 
-    admin
-  })
-
-  await usersRepository.save(user);
-
-  return user;
- }
 }
 
-export { CreateUser }
+export { CreateUser };
