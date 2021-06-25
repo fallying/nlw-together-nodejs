61d8444bb94bb83ce2055f425aebd2e2203ad6dc
fallying <jonasexplore@gmail.com>
2021-06-25 13:48:08 -0300

    feat(infra): added instance of list user

--- src/infra/http/factories/controllers/UserController.ts
+++ src/infra/http/factories/controllers/UserController.ts
@@ -1,7 +1,13 @@
 import { CreateUserController } from "../../../../modules/users/useCases/CreateUser";
 import { AuthenticateUserController } from "../../../../modules/users/useCases/AuthenticateUser";
+import { ListUsersController } from "../../../../modules/users/useCases/ListUsers";
 
 const makeCreateUserController = new CreateUserController();
 const makeAuthenticateUserController = new AuthenticateUserController();
+const makeListUsersController = new ListUsersController();
 
-export { makeCreateUserController, makeAuthenticateUserController };
+export {
+  makeCreateUserController,
+  makeAuthenticateUserController,
+  makeListUsersController,
+};
