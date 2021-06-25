ee6ce7108b448b21a2e066fbbde7e6c1242efd45
fallying <jonasexplore@gmail.com>
2021-06-25 13:15:36 -0300

    refactor(infra): receive user_id by request and validate inforamtions

--- src/infra/http/middlewares/ensureAdmin.ts
+++ src/infra/http/middlewares/ensureAdmin.ts
@@ -1,17 +1,21 @@
 import { NextFunction, Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { UsersRepositories } from "../../../modules/users/repositories";
 
-export function ensureAdmin(
+export async function ensureAdmin(
   request: Request,
   response: Response,
   _next: NextFunction
 ) {
-  const admin = false;
+  const { user_id } = request;
+
+  const usersRepositories = getCustomRepository(UsersRepositories);
+
+  const { admin } = await usersRepositories.findOne(user_id);
 
   if (admin) {
     return _next();
   }
 
-  return response.status(401).json({
-    error: "Unauthorized",
-  });
+  return response.status(401).end();
 }
