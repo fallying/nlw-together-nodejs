7e56643b53312b8971a240a0e6b423a96ce7eb67
fallying <jonasexplore@gmail.com>
2021-06-25 13:16:10 -0300

    feat(routes): added routes to list compliments by user

--- src/infra/http/routes/users.routes.ts
+++ src/infra/http/routes/users.routes.ts
@@ -1,8 +1,23 @@
 import { Router } from "express";
+import {
+  makeListUserComplimentsReceivedController,
+  makeListUserComplimentsSentController,
+} from "../factories/controllers/UserComplimentsController";
 import { makeCreateUserController } from "../factories/controllers/UserController";
+import { ensureAuthenticated } from "../middlewares";
 
 const userRouter = Router();
 
 userRouter.post("/", makeCreateUserController.handle);
+userRouter.get(
+  "/compliments/received",
+  ensureAuthenticated,
+  makeListUserComplimentsReceivedController.handle
+);
+userRouter.get(
+  "/compliments/sent",
+  ensureAuthenticated,
+  makeListUserComplimentsSentController.handle
+);
 
 export { userRouter };
